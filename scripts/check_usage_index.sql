4f2c19b309a2b2d42d92d289e51ff1851e647728
Sérgio Passos <sergio.passos88@gmail.com>
2019-09-26 13:43:52 -0300

    Scripts

---
+++ scripts/check_usage_index.sql
@@ -0,0 +1,10 @@
+select objname = OBJECT_NAME(s.object_id),
+s.object_id,
+index_name= i.name,
+index_id = i.index_id,
+user_seeks, user_scans, user_lookups
+from sys.dm_db_index_usage_stats as s
+join sys.indexes i on i.object_id = s.object_id and i.index_id = s.index_id
+where database_id = DB_ID('DEVECI')
+and OBJECTPROPERTY(s.object_id,'IsUserTable')=1
+order by (user_seeks + user_scans + user_lookups) desc;
